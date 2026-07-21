d01bb84a31b757a66768f3d1c39ecd630c747118
Aarush Kalia <aarushkalia@Aarushs-MacBook-Air-9.local>
2026-07-20 21:55:53 -0400

    basic RAG complete

--- chat.py
+++ chat.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
+from llama_index.core import VectorStoreIndex
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.google_genai import GoogleGenAI
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
 
 
 load_dotenv()
@@ -11,5 +14,18 @@ llm = GoogleGenAI(
     model="gemini-3.5-flash",
 )
 
-resp = llm.complete("what are the policy objectives of the central bank?")
-print(resp)
+db = chromadb.PersistentClient(path="./chroma_db")
+chroma_collection = db.get_or_create_collection("my_notes")
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+index = VectorStoreIndex.from_vector_store(
+    vector_store,
+    embed_model=embed_model,
+)
+
+query_engine = index.as_query_engine(llm=llm)
+response = query_engine.query("what are the policy objectives of the central bank?")
+print(response)
+
+response = query_engine.query("How much does the Gray Whale cost")
+print(response)
